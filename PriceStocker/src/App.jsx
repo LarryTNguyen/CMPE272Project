a75f1535e5fbd557a59cee467ead40a716ac307f
Nyan Ye Lin <nyanyyelin@gmail.com>
2025-10-15 14:09:37 -0700

    added pages and routing

--- PriceStocker/src/App.jsx
+++ PriceStocker/src/App.jsx
@@ -1,5 +1,45 @@
-function App() {
-  return <div>Hello </div>;
-}
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import AppLayout from './pages/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" replace />,
+      },
+      {
+        path: '/home',
+        element: <Home />,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
