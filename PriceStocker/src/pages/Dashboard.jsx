7c6c4253066047e9ed355ef29cd2ef2b6bc02ff0
Nyan Ye Lin <nyanyyelin@gmail.com>
2025-11-14 00:21:00 -0800

    moved buy/sell to dashboard

--- PriceStocker/src/pages/Dashboard.jsx
+++ PriceStocker/src/pages/Dashboard.jsx
@@ -1,5 +1,29 @@
+// import AddFunds from '../components/AddFunds';
+import BuyForm from '../components/portfolio/BuyForm';
+import Positions from '../components/portfolio/Positions';
+import Modal from '../components/Modal';
+import { createPortal } from 'react-dom';
+import { useState } from 'react';
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => setIsOpen(true)}
+        className="rounded-lg bg-blue-500 p-2"
+      >
+        Buy Stock
+      </button>
+      <Positions />
+      {isOpen &&
+        createPortal(
+          <Modal open={isOpen} onClose={() => setIsOpen(false)}>
+            <BuyForm onClose={() => setIsOpen(false)} />
+          </Modal>,
+          document.body,
+        )}
+    </>
+  );
 };
 
 export default Dashboard;
--- PriceStocker/src/pages/Home.jsx
+++ PriceStocker/src/pages/Home.jsx
@@ -1,29 +1,5 @@
-// import AddFunds from '../components/AddFunds';
-import BuyForm from '../components/portfolio/BuyForm';
-import Positions from '../components/portfolio/Positions';
-import Modal from '../components/Modal';
-import { createPortal } from 'react-dom';
-import { useState } from 'react';
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <>
-      <button
-        onClick={() => setIsOpen(true)}
-        className="rounded-lg bg-blue-500 p-2"
-      >
-        Buy Stock
-      </button>
-      <Positions />
-      {isOpen &&
-        createPortal(
-          <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-            <BuyForm onClose={() => setIsOpen(false)} />
-          </Modal>,
-          document.body,
-        )}
-    </>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
